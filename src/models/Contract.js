6af33708ed685a48b50203cc106799c58143f8f6
Biraj Sapkota <beerazchelsea23@gmail.com>
2023-01-06 19:56:47 +0545

    added contract model

---
+++ src/models/Contract.js
@@ -0,0 +1,27 @@
+const mongoose = require("mongoose");
+
+const ContractSchema = new mongoose.Schema(
+  {
+    room: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "Room",
+    },
+    owner: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "User",
+    },
+    tenant: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "User",
+    },
+    agreementDetails: {
+      type: String,
+      required: [true, "agreement details is required"],
+    },
+  },
+  {
+    timestamps: true,
+  }
+);
+
+module.exports = mongoose.model("bill", ContractSchema);
