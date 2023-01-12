7c4ae8d0dcda5565f96d7312e4e0adf294b2ef92
Biraj Sapkota <beerazchelsea23@gmail.com>
2023-01-12 19:16:29 +0545

    added room-booking model

---
+++ src/models/RoomBooking.js
@@ -0,0 +1,27 @@
+const mongoose = require("mongoose");
+
+const RoomBookingSchema = new mongoose.Schema(
+  {
+    room: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "Room",
+    },
+    user: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "User",
+    },
+    contract: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "Contract",
+    },
+    transaction: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "Transaction",
+    },
+  },
+  {
+    timestamps: true,
+  }
+);
+
+module.exports = mongoose.model("roomBooking", RoomBookingSchema);
