b8e05f8c508f2e136988e1130b878e602b0efbe4
Biraj Sapkota <beerazchelsea23@gmail.com>
2023-01-18 14:16:22 +0545

    edited db connection

--- src/config/dbConnection.js
+++ src/config/dbConnection.js
@@ -2,9 +2,18 @@ const mongoose = require('mongoose')
 require('dotenv').config()
 
 mongoose.set('strictQuery', false);
+
 const connectToMongo = () => {
-    mongoose.connect(process.env.mongoURI, () => {
-        console.log("Database connection successful.")
+    mongoose.connect(process.env.mongoURI, {
+        useNewUrlParser: true
+    })
+    .then(() => {
+        console.log("Successful Connection to Database.")
+        
+    })
+    .catch((_err) => {
+        console.log("Can't Connect to Database!!")
+        process.exit()
     })
 }
 
