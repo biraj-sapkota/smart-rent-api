f1d428408786e4539c92c16091fd66603c18f83c
Biraj Sapkota <beerazchelsea23@gmail.com>
2023-01-25 23:20:52 +0545

    managed cors

--- src/app.js
+++ src/app.js
@@ -1,9 +1,12 @@
 const express = require("express");
+const cors = require("cors");
 const routesSetup = require("./routes/index");
+const corsOptions = require("./config/corsOptions");
 
 const configureExpress = () => {
   const app = express();
 
+  app.use(cors(corsOptions));
   app.use(express.json());
   app.use(express.urlencoded({ extended: true }));
 
---
+++ src/config/corsOptions.js
@@ -0,0 +1,5 @@
+exports.corsOptions = {
+  origin: "*",
+  credentials: true,
+  optionsSuccessStatus: 200,
+};
