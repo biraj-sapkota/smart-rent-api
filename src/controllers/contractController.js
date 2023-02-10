2df35ea3979d4ac385896418f40049905c4d76e1
Biraj Sapkota <beerazchelsea23@gmail.com>
2023-02-10 20:08:02 +0545

    design contract controller

---
+++ src/controllers/contractController.js
@@ -0,0 +1,40 @@
+const Contract = require("../models/Contract");
+
+exports.createContract = (req, res) => {
+  const contract = new Contract(req.body);
+
+  contract
+    .save()
+    .then((data) => {
+      res.send(data);
+    })
+    .catch((_err) => {
+      res.status(500).send("Error creating the contract.");
+    });
+};
+
+exports.updateContract = (req, res) => {
+  Contract.findOneAndUpdate(
+    { _id: req.params.contractID },
+    req.body,
+    { new: true, useFindAndModify: false },
+    (err, data) => {
+      if (err) res.status(500).send(err);
+      res.json(data);
+    }
+  );
+};
+
+exports.deleteContract = (req, res) => {
+  Contract.deleteOne({ _id: req.params.contractID }, (err) => {
+    if (err) res.status(500).send(err);
+    res.send("Contract Deleted Successfully!!");
+  });
+};
+
+exports.getContract = (_req, res) => {
+  Contract.find({}, (err, data) => {
+    if (err) res.status(500).send(err);
+    res.json(data);
+  });
+};
